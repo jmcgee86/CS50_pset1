98b06ed08b4e1052b8a08d8de85a30eb1cc9865a
Jim McGee <jmcgee86@gmail.com>
2018-04-09 17:59:37 +0000

    add comments for cash.c

--- cash/cash.c
+++ cash/cash.c
@@ -13,28 +13,28 @@ int main(void)
     }
     while (changefloat < 0);
 
-    changeround = round(changefloat * 100);
+    changeround = round(changefloat * 100); //rounds change and converts to integer
 
-    while (changeround >= 25)
+    while (changeround >= 25) //while change owed is great than or equal to 25, subtracts 25 and adds one coin
     {
         changeround -= 25;
         coins++;
     }
 
-    while (changeround >= 10)
+    while (changeround >= 10) //while change owed is great than or equal to 10, subtracts 10 and adds one coin
     {
         changeround -= 10;
         coins++;
     }
-    while (changeround >= 5)
+    while (changeround >= 5) //while change owed is great than or equal to 10, subtracts 10 and adds one coin
     {
         changeround -= 5;
         coins++;
     }
-    while (changeround >= 1)
+    while (changeround >= 1) //while change owed is great than or equal to 10, subtracts 10 and adds one coin
     {
         changeround -= 1;
         coins++;
     }
-    printf("%i\n", coins);
+    printf("%i\n", coins); //when changed owed is less than one, prints coins int, which is number fewest number of coins needed for change
 }
